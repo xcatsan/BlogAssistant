9cbe7825dbd2b598ddcb9965999728150a17b300
Hiroshi Hashiguchi <xcatsan@mac.com>
2010-01-22 17:55:20 +0900

    Separated plugin and application

---
+++ AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestAppController.h
+//  BlogAssistant
+//
+//  Created by Hiroshi Hashiguchi on 09/12/25.
+//  Copyright 2009 xcatsan.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class ViewerController;
+@interface AppController : NSObject {
+
+	IBOutlet NSArrayController* arrayController;
+	ViewerController* viewerController;
+}
+
+-(IBAction)save:(id)sender;
+
+@end
---
+++ AppController.m
@@ -0,0 +1,51 @@
+//
+//  TestAppController.m
+//  BlogAssistant
+//
+//  Created by Hiroshi Hashiguchi on 09/12/25.
+//  Copyright 2009 xcatsan.com. All rights reserved.
+//
+
+#import "AppController.h"
+#import "CoreDataManager.h"
+#import "ModelManager.h"
+#import "ViewerController.h"
+
+@implementation AppController
+
+#pragma mark -
+#pragma mark Initialization and Deallocation
+-(id)init
+{
+	self = [super init];
+	
+	if (self) {
+	}
+	
+	return self;
+}
+
+-(void)awakeFromNib
+{
+	[arrayController setManagedObjectContext:
+		[[CoreDataManager sharedManager] managedObjectContext]];
+	viewerController = [[ViewerController alloc] init];
+	[viewerController window];
+}
+
+- (void) dealloc
+{
+	[viewerController release];
+	[super dealloc];
+}
+
+
+#pragma mark -
+#pragma mark Event handler
+
+-(IBAction)save:(id)sender
+{
+	[[ModelManager sharedManager] save];
+}
+
+@end
